a4b08b0128446b5eae52fae1fd3cbec3af348197
Jason Yu <jasonyu@Jasons-MacBook-Pro.local>
2016-01-07 15:23:01 -0500

    Jason Edits

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,11 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+$("#main").append("Jason Yu");
+
+var formattedName = HTMLheaderName.replace("%data%", "Jason Yu");
+var formattedRole = HTMLheaderRole.replace("%data%", "UX Designer/Front-End Developer")
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
